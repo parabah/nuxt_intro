5c81ae2eea39b8186e646194ca6a0f2239adf4f6
parabah <parabah.dev@gmail.com>
2022-04-04 20:32:33 -0300

    Aula 08-ErrorPage

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Ocorreu um erro</h1>
+      <h2 v-if="error.statusCode === 404">Página não encontrada</h2>
+      <h2 v-else>{{ error.message }}</h2>
+      <nuxt-link to="/">Voltar ao início</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+    layout: 'no-nav',
+    props:{
+        error: {
+            type: Object,
+            default: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
