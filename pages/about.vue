ab10dd20fa5afc7439ea84c796d8434456d4a630
parabah <parabah.dev@gmail.com>
2022-04-22 20:25:16 -0300

    18-Interface TailwindCSS parte2

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,52 @@
 <template>
   <div>
-    <h1 class="text-2xl text-center">About</h1>
-    <p class="lobster">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, quae mollitia esse distinctio rem reprehenderit? Earum consectetur facere commodi doloremque nulla, delectus sit at, temporibus accusantium necessitatibus laudantium consequatur quae!</p>
+    <div class="flex max-w-2xl rounded-lg shadow-md m-2">
+        <div class="bg-blue-400 w-1/5 rounded-tl-lg rounded-bl-lg">
+            <img :src="aboutData.avatar" alt="" class="p-1">            
+        </div>
+        <div class="w-4/5">
+            <h1 class="py-2 bg-blue-900 text-white text-center font-bold text-2xl rounded-tr-lg">{{aboutData.name}}</h1>
+            <a 
+             :href="`mailto:${aboutData.email}`"
+             class="text-sm font-thin pl-2"
+            >{{aboutData.email}}</a>
+            <p class="text-xs font-mono p-2">{{aboutData.bio}}</p>
+            <div class="flex justify-end text-lg mr-2">
+              <a :href="aboutData.facebook" target="_blank" class="px-1 fa fa-facebook"></a>
+              <a :href="aboutData.twitter" target="_blank" class="px-1 fa fa-twitter"></a>
+              <a :href="aboutData.instagram" target="_blank" class="px-1 fa fa-instagram"></a>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AboutPage',
+  props: {
+    aboutData : {
+      type: Object,
+      default() {
+        return {
+          name : 'Meu nome vai aqui',
+          avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Mug_icon.png/640px-Mug_icon.png',
+          bio: 'Esta é minha biografia',
+          email: 'endereco@mail.com',
+          facebook: 'https://www.facebook.com/meu_usuario',
+          instagram: 'https://www.instagram.com/meu_usuario/',
+          twitter: 'https://twitter.com/meu_usuario'
+        }
+      }
+    }
+  },
+  head() {
+    return {
+      link: [ 
+        { rel:"stylesheet",  href :"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" }
+      ]
+    }
+  }
 }
 </script>
 <style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <h1 class="text-2xl text-center">Login</h1>
-    <nuxt-link to="/">Início</nuxt-link>
+  <div class="flex flex-col  items-center my-4 mx-auto w-4/5 border rounded-md">
+    <label class="font-bold">e-mail
+      <input id="email" v-model="user.email" class="border mt-4 p-1" type="text" name="email" />
+    </label>
+    <label class="font-bold">senha
+      <input id="senha" class="border mt-4 p-1" type="password" name="senha" />
+    </label>
+    <div class="flex justify-center w-4/5 m-2 ">
+        <nuxt-link 
+          to="/"
+          class="w-32 m-2 p-2 rounded-lg bg-blue-400 text-center text-white"
+        >Login</nuxt-link>
+        <button
+          class="w-32 m-2 p-2 rounded-lg border"
+          @click="cancelar"
+        >Cancelar</button>
+    </div>
   </div>
 </template>
 
@@ -39,6 +53,15 @@ export default {
       const dia = hoje.getDate();
       const mes = hoje.getMonth() + 1;
       return (dia < 9 ? '0'+ dia : dia) + '/' + (mes < 9 ? '0'+ mes : mes) + '/' + hoje.getFullYear();
+    },
+    user(){
+      return this.$store.state.user;
+    }
+  },
+  methods: {
+    cancelar(){
+      this.$store.dispatch('setUser', {});
+      this.$router.push('/');
     }
   }
 }
